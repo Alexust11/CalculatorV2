910aca994abefc156f046cb27e43079f14655f41
Алексей <alexust11@yandex.ru>
2022-11-23 14:34:46 +0700

    Все методы без проверок

--- src/main/java/com/example/calculatorv2/Controller/FirstController.java
+++ src/main/java/com/example/calculatorv2/Controller/FirstController.java
@@ -25,5 +25,20 @@ public class FirstController {
     public String plus(@RequestParam Integer num1, @RequestParam Integer num2) {
                  return num1 + "+ " + num2 + " = " +calcService.plus(num1,num2) ;
     }
+    @GetMapping("/minus")
 
+    public String minus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + "- " + num2 + " = " +calcService.minus(num1,num2) ;
+    }
+    @GetMapping("/multiply")
+
+    public String multiply(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + "* " + num2 + " = " +calcService.multiply(num1,num2) ;
+    }
+
+    @GetMapping("/divide")
+
+    public String divide(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + "/ " + num2 + " = " +calcService.divide(num1,num2) ;
+    }
 }
